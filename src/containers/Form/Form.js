72e86ceda9eb5d8dd13f4a77026b7daed5fcb8f5
Ed Ignot <edignot@gmail.com>
2020-11-22 11:26:35 -0700

    Search input created and styled

--- src/containers/Form/Form.js
+++ src/containers/Form/Form.js
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { BiSearch } from 'react-icons/bi'
+import './Form.css'
 
 const Form = () => {
-  return <div>Form</div>
+  const dispatch = useDispatch()
+
+  const [searchValue, setSearchValue] = useState('')
+
+  // useEffect(() => {
+  //   searchValue === '' && dispatch(clearSearch())
+  // }, [searchValue])
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value)
+  }
+
+  const handleKeyDown = (e) => {
+    e.key === 'Enter' && searchHandler()
+  }
+
+  const searchHandler = () => {
+    alert('handle change')
+  }
+
+  return (
+    <section className='search-input-wrapper'>
+      <input
+        name='Search by Movie Title'
+        type='text'
+        placeholder='Search by Movie Title'
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+        value={searchValue}
+        className='search-input'
+        data-testid='search-input'
+      />
+      <button onClick={searchHandler} className='search-button'>
+        <BiSearch />
+      </button>
+    </section>
+  )
 }
 
 export default Form
