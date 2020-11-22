028807bbdd57b56bb9be1974f9fc58d0b9a9c460
Ed Ignot <edignot@gmail.com>
2020-11-22 11:44:41 -0700

    Movies component structure added

--- src/containers/Movies/Movies.js
+++ src/containers/Movies/Movies.js
@@ -1,18 +1,47 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getMovies } from '../../actions/movies'
+import Movie from '../Movie/Movie'
+import LoadingSpinner from '../../components/LoadingSpinner/LoadingSpinner'
+import Pagination from '../Pagination/Pagination'
+import './Movies.css'
 
 const Movies = () => {
   const dispatch = useDispatch()
 
+  const [loading, setLoading] = useState(false)
+
   useEffect(() => {
     const fetchMovies = async () => {
+      setLoading(true)
       await dispatch(getMovies(1))
+      setLoading(false)
     }
     fetchMovies()
   }, [dispatch])
 
-  return <div>Movies</div>
+  const mappedMovies = []
+
+  return (
+    <section className='movies-pagination-container'>
+      <section className='restaurants-container'>
+        {mappedMovies.length ? (
+          mappedMovies
+        ) : (
+          <p>No movies matching search result found...</p>
+        )}
+      </section>
+
+      {/* <Pagination /> */}
+      {loading && <LoadingSpinner asOverlay />}
+      {/* update pagination values */}
+      <Pagination
+        moviesPerPage={20}
+        totalMovies={70}
+        // paginate={paginateHandler}
+      />
+    </section>
+  )
 }
 
 export default Movies
