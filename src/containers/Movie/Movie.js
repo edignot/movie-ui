88efd761b33dbccbae80160e15907a8b6fe311ed
Ed Ignot <edignot@gmail.com>
2020-11-22 11:44:20 -0700

    Movie component initialized

---
+++ src/containers/Movie/Movie.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa'
+
+const Movie = () => {
+  return (
+    <section>
+      <FaThumbsDown />
+      <FaThumbsUp />
+    </section>
+  )
+}
+
+export default Movie
