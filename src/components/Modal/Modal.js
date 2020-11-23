b617616ae8da080f85d922b16319c93bc71189d1
Ed Ignot <edignot@gmail.com>
2020-11-22 21:42:13 -0700

    Modal with movie details created

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import './Modal.css'
+
+const Modal = ({ show, children }) => {
+  const showOrHide = show ? 'modal display-block' : 'modal display-none'
+
+  return (
+    <section className={showOrHide}>
+      <section className='modal-main'>{children}</section>
+    </section>
+  )
+}
+
+export default Modal
+
+Modal.propTypes = {
+  children: PropTypes.node,
+}
--- src/containers/Movie/Movie.js
+++ src/containers/Movie/Movie.js
@@ -1,20 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaThumbsDown, FaThumbsUp } from 'react-icons/fa'
+import Modal from '../../components/Modal/Modal'
 import './Movie.css'
 
 const Movie = ({ movie }) => {
-  const { id, title, poster_path } = movie
-  return (
-    <section className='movie-container'>
-      <FaThumbsDown className='up-vote-icon' />
-      <FaThumbsUp className='down-vote-icon' />
+  const { id, title, poster_path, backdrop_path } = movie
 
-      <img
-        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-        className='movie-image'
-        alt={title}
-      />
-    </section>
+  const [displayMovieInfo, setDisplayMovieInfo] = useState(false)
+
+  const handleDisplayMovieInfo = () => {
+    setDisplayMovieInfo(true)
+  }
+
+  return (
+    <>
+      <section className='movie-container'>
+        <FaThumbsDown className='up-vote-icon' />
+        <FaThumbsUp className='down-vote-icon' />
+
+        <img
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          className='movie-image'
+          alt={title}
+          onClick={handleDisplayMovieInfo}
+        />
+      </section>
+      <Modal show={displayMovieInfo}>
+        <img src={`https://image.tmdb.org/t/p/w500${backdrop_path}`} />
+        <p>{title}</p>
+      </Modal>
+    </>
   )
 }
 
